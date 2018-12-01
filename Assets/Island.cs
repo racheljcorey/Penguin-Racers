e1277e51c4e3d85293f76315412c8aa6c35bd3d5
racheljcorey <racheljcorey@gmail.com>
2018-12-01 16:14:49 -0700

    /

---
+++ Assets/Island.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island : MonoBehaviour {
+
+    private bool islandSelected;
+
+    private void OnMouseDown()
+    {
+        islandSelected = true;
+
+        Debug.Log("test");
+    }
+
+
+}
