26eb0c073f161a370dd4dfbe47f4798063a551fa
racheljcorey <racheljcorey@gmail.com>
2018-12-01 18:00:52 -0700

    Update DetectClicks.cs

--- Assets/DetectClicks.cs
+++ Assets/DetectClicks.cs
@@ -1,32 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using cakeslice;
 
 public class DetectClicks : MonoBehaviour {
 
     public bool clickedObject;
     private Ray ray;
     private RaycastHit hit;
+    private GameObject[] islands;
+    private GameObject island;
+
 
 	// Use this for initialization
 	void Start () {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        islands = GameObject.FindGameObjectsWithTag("Island");
+        island = GameObject.FindGameObjectWithTag("Island");
+        
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetMouseButtonDown(0))
         {
-            if (hit.transform.gameObject.tag == "Island")
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Island")
             {
-                Debug.Log("This is an Island");
+                    Debug.Log("This is an Island");
+                clickedObject = true;
             }
             else
             {
                 Debug.Log("This isn't an Island");
+                clickedObject = false;
+            }
+
+            //if (!clickedObject)
+            //{
+            //    foreach (GameObject island in islands)
+            //    {
+            //        island.GetComponent<Outline>().eraseRenderer = true;
+            //    }
+            //}
+
+            if (island.GetComponent<Outline>() != null)
+            {
+                Debug.Log("test");
             }
         }
+
+
+
 
     }
 }
